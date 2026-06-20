75c0e6397212205a0ff45e9d991168e27e7ca95b
evertide <Evertide001@gmail.com>
2026-06-20 17:17:34 -0500

    Making work lol

--- src/main/java/net/Evertide/backroomsbackstage/BackroomsBackstageClient.java
+++ src/main/java/net/Evertide/backroomsbackstage/BackroomsBackstageClient.java
@@ -1,5 +1,6 @@
-package com.example.examplemod;
+package net.Evertide.backroomsbackstage;
 
+import net.Evertide.Backrooms.BackroomsBackstage;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -11,11 +12,11 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = BackroomsBackstage.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
-public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+@EventBusSubscriber(modid = BackroomsBackstage.MODID, value = Dist.CLIENT)
+public class BackroomsBackstageClient {
+    public BackroomsBackstageClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -25,7 +26,7 @@ public class ExampleModClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        BackroomsBackstage.LOGGER.info("HELLO FROM CLIENT SETUP");
+        BackroomsBackstage.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
